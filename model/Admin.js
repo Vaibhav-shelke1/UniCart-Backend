80ce1b7c143f89d6f09588547207ed9386a9c313
Vaibhav-shelke1 <shelkevaibhav218@gmail.com>
2024-10-05 15:03:10 +0530

    admin Schema added

---
+++ model/Admin.js
@@ -0,0 +1,23 @@
+import { Schema } from "mongoose";
+
+const adminSchema=new Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true, 
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true 
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+const Admin=mongoose.model("Admin", adminSchema);
+export default Admin;
